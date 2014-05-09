ec84192d4d0e896f476adbde61c921163baa6a31
Seth Vargo <sethvargo@gmail.com>
2014-05-08 23:46:34 -0400

    Update Rubocop madness

--- lib/omnibus/project.rb
+++ lib/omnibus/project.rb
@@ -156,7 +156,7 @@ module Omnibus
     #   must be set in order to build a project)
     def name(val = NULL_ARG)
       @name = val unless val.equal?(NULL_ARG)
-      @name || fail(MissingProjectConfiguration.new('name', 'my_project'))
+      @name || raise(MissingProjectConfiguration.new('name', 'my_project'))
     end
 
     # Set or retrieve a friendly name for the project
@@ -178,7 +178,7 @@ module Omnibus
     def msi_parameters(val = NULL_ARG, &block)
       if block_given?
         unless val.equal?(NULL_ARG)
-          fail 'can not specify additional parameters when block is given'
+          raise 'can not specify additional parameters when block is given'
         end
 
         @msi_parameters = block
@@ -220,7 +220,7 @@ module Omnibus
         @install_path = File.expand_path(val)
         windows_safe_path!(@install_path)
       end
-      @install_path || fail(MissingProjectConfiguration.new('install_path', '/opt/chef'))
+      @install_path || raise(MissingProjectConfiguration.new('install_path', '/opt/chef'))
     end
 
     # Set or retrieve the the package maintainer.
@@ -233,7 +233,7 @@ module Omnibus
     #   be set in order to build a project)
     def maintainer(val = NULL_ARG)
       @maintainer = val unless val.equal?(NULL_ARG)
-      @maintainer || fail(MissingProjectConfiguration.new('maintainer', 'Chef Software, Inc.'))
+      @maintainer || raise(MissingProjectConfiguration.new('maintainer', 'Chef Software, Inc.'))
     end
 
     # Set or retrive the package homepage.
@@ -246,7 +246,7 @@ module Omnibus
     #   set in order to build a project)
     def homepage(val = NULL_ARG)
       @homepage = val unless val.equal?(NULL_ARG)
-      @homepage || fail(MissingProjectConfiguration.new('homepage', 'http://www.getchef.com'))
+      @homepage || raise(MissingProjectConfiguration.new('homepage', 'http://www.getchef.com'))
     end
 
     # Defines the iteration for the package to be generated.  Adheres
